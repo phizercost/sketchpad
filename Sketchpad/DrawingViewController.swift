3dcc259314bc3c3626af9d7901a5a4c08e96a6af
phizercost <phizer.development@gmail.com>
2019-06-17 12:16:02 +0200

    Adding drawing tools

--- Sketchpad/DrawingViewController.swift
+++ Sketchpad/DrawingViewController.swift
@@ -7,16 +7,67 @@
 //
 
 import UIKit
+import ChromaColorPicker
 
-class DrawingViewController: UIViewController {
+class DrawingViewController: UIViewController, ChromaColorPickerDelegate {
+    
 
     @IBOutlet weak var imageView: UIImageView!
     var lastPoint: CGPoint = CGPoint(x:0, y:0)
+    var currentColor = UIColor.blue.cgColor
+    var brushSize: Float = 30.0
+    var colorPicker: ChromaColorPicker?
+    var greyedOut = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        greyedOut = UIView(frame: view.frame)
+        greyedOut.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        view.addSubview(greyedOut)
 
-        // Do any additional setup after loading the view.
+        colorPicker = ChromaColorPicker(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        if let picker = colorPicker {
+            picker.delegate = self
+            picker.padding = 5
+            picker.stroke = 3
+            picker.hexLabel.isHidden = true
+            view.addSubview(picker)
+        }
+        colorPicker?.isHidden = true
+        greyedOut.isHidden = true
+        
     }
+    func colorPickerDidChooseColor(_ colorPicker: ChromaColorPicker, color: UIColor) {
+        currentColor = color.cgColor
+        colorPicker.isHidden = true
+        greyedOut.isHidden = true
+    }
+    
+    @IBAction func colorTapped(_ sender: Any) {
+        colorPicker?.adjustToColor(UIColor(cgColor: currentColor))
+        colorPicker?.isHidden = false
+         greyedOut.isHidden = false
+    }
+    
+    @IBAction func sizeTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Brush Size", message: "\n\n\n\n\n\n\n", preferredStyle: .alert)
+        let slider = UISlider(frame: CGRect(x: 10, y: 50, width: 250, height: 80))
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.value = brushSize
+        alert.view.addSubview(slider)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (al) in
+            self.brushSize = slider.value
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func eraseTapped(_ sender: Any) {
+        currentColor = UIColor.white.cgColor
+    }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let beginPoint = touches.first?.location(in: imageView) {
@@ -44,9 +95,9 @@ class DrawingViewController: UIViewController {
         if let context = UIGraphicsGetCurrentContext() {
             context.move(to: point1)
             context.addLine(to: point2)
-            context.setLineWidth(10)
+            context.setLineWidth(CGFloat(brushSize) / 3.0)
             context.setLineCap(.round)
-            context.setStrokeColor(UIColor.red.cgColor)
+            context.setStrokeColor(currentColor)
             context.strokePath()
             imageView.image = UIGraphicsGetImageFromCurrentImageContext()
         }
